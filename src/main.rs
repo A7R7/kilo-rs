538f2162d6be5d8b7a94b5df15c0e910f9836055
a7r7 <Aaron__Lee_@outlook.com>
2025-03-03 16:06:08 +0800

    enable raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use std::io::{self, Read};
+use std::os::unix::io::AsFd;
+use nix::sys::termios::{tcgetattr, tcsetattr, LocalFlags, SetArg};
+
+fn enable_raw_mode() -> io::Result<()> {
+    let stdin = io::stdin();
+    let fd = stdin.as_fd();
+    let mut termios = tcgetattr(fd).map_err(io::Error::from)?;
+
+    termios.local_flags.remove(LocalFlags::ECHO);
+
+    tcsetattr(fd, SetArg::TCSAFLUSH, &termios).map_err(io::Error::from)?;
+    Ok(())
+}
 
 fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+
     let mut buffer = [0; 1];
     while io::stdin().read(&mut buffer)? == 1 && buffer[0] != b'q' {}
     Ok(())
