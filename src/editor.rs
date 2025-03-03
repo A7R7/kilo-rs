58191e2c801408f4547ef74c8d7eedf0167ab0d7
a7r7 <Aaron__Lee_@outlook.com>
2025-03-03 20:12:06 +0800

    refactor keyboard input

--- src/editor.rs
+++ src/editor.rs
@@ -1,12 +1,31 @@
- 
-fn editor_read_key() {
+use std::io::{self, Read, Write};
+use std::process::exit;
+use anyhow::{Result, Context};
 
+macro_rules! ctrl_key {
+    ($k:expr) => {($k as u8 & 0x1f) as u8};
 }
 
-fn editor_process_keypress() {
-
+fn read_key() -> Result<u8> {
+    let mut buffer = [0u8; 1];
+    loop {
+        match io::stdin().read(&mut buffer) {
+            Ok(_) => return Ok(buffer[0]),
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+            Err(e) => return Err(e).context("Failed to read key from stdin"),
+        }
+    }
 }
 
-fn editor_refresh_screen() {
+pub fn process_keypress() -> Result<()>{
+    let c = read_key()?;
+    match c {
+        c if c == ctrl_key!('q') => exit(0),
+        _ => {},
+    };
+    Ok(())
+}
 
+pub fn refresh_screen() {
+    io::stdout().write_all(b"\x1b[2J").unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
 mod raw_mode;
 mod editor;
 
-use std::io::{self, Read};
-use anyhow::{Context, Result};
+use anyhow::Result;
 
 fn main() -> Result<()> {
     raw_mode::enable_raw_mode()?;
 
-    let mut buffer = [0u8; 1];
     loop {
-        buffer[0] = '\0' as u8;
-        io::stdin().read(&mut buffer)
-            .context("Failed to read input")?;
-        let byte = buffer[0] as char;
-        if byte.is_control() {
-            print!("{}\r\n", byte as u8);
-        } else {
-            print!("{} ('{}')\r\n", byte as u8, byte);
-        }
-        if byte == 'q' {
-            break;
-        }
+        editor::process_keypress()?
     }
-    Ok(())
 }
