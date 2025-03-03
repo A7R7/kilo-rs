de173a35d0aac868f5e10e88583142def8806a26
a7r7 <Aaron__Lee_@outlook.com>
2025-03-03 16:21:05 +0800

    use anyhow to handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use std::io::{self, Read};
 use std::os::unix::io::AsFd;
 use nix::sys::termios::{tcgetattr, tcsetattr, LocalFlags, SetArg};
+use anyhow::{Context, Result};
 
-fn enable_raw_mode() -> io::Result<()> {
+fn enable_raw_mode() -> Result<()> {
     let stdin = io::stdin();
     let fd = stdin.as_fd();
-    let mut termios = tcgetattr(fd).map_err(io::Error::from)?;
+    let mut termios = tcgetattr(fd)
+        .context("Failed to get terminal attributes")?;
 
     termios.local_flags.remove(LocalFlags::ECHO);
 
-    tcsetattr(fd, SetArg::TCSAFLUSH, &termios).map_err(io::Error::from)?;
+    tcsetattr(fd, SetArg::TCSAFLUSH, &termios)
+        .context("Failed to set terminal attributes")?;
     Ok(())
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     enable_raw_mode()?;
 
     let mut buffer = [0; 1];
