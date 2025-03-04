67ca51bcf0503b28c779fb47262693c5541f05f6
a7r7 <Aaron__Lee_@outlook.com>
2025-03-05 01:46:41 +0800

    clear lines instead of entire screen

--- src/output.rs
+++ src/output.rs
@@ -12,9 +12,12 @@ impl Editor {
     fn draw_rows_str(&self) -> String {
         let mut str = String::new();
         for _ in 1..= (self.screenrows - 1) {
-            str.push_str("~\r\n");
+            str.push_str("~");
+            str.push_str(CLEAR_LINE_CMD);
+            str.push_str("\r\n")
         }
         str.push_str("~");
+        str.push_str(CLEAR_LINE_CMD);
         str
     }
 
@@ -38,7 +41,6 @@ impl Editor {
     pub fn refresh_screen(&self) {
         let mut buf  = String::new();
         buf.push_str(HIDE_CURSOR_CMD);
-        buf.push_str(CLEAR_SCREEN_CMD);
         buf.push_str(REPOSITION_CURSOR_CMD);
         buf.push_str(self.draw_rows_str().as_str());
         buf.push_str(REPOSITION_CURSOR_CMD);
